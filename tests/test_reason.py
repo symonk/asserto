847a4e403da42599c57ed560fe80d9d15fd76543
symonk <jackofspaces@gmail.com>
2022-04-24 17:20:06 +0100

    gap in coverage for `descriptions`

--- tests/test_reason.py
+++ tests/test_reason.py
@@ -19,3 +19,10 @@ def test_described_as(asserto) -> None:
     with pytest.raises(AssertionError) as error:
         asserto(1).described_as("foo!").is_equal_to(2)
     asserto(error.value.args[0]).is_equal_to("foo!")
+
+
+@pytest.mark.filterwarnings("ignore::asserto._warnings.NoAssertAttemptedWarning")
+def test_described_updates_reason(asserto) -> None:
+    foo = "foo"
+    x = asserto(1).described_as(foo)
+    asserto(x._reason.description).is_equal_to(foo)
