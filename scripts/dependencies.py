74d2acbdd65faeac537996c7b795f48e6bffa0b9
symonk <jackofspaces@gmail.com>
2022-05-02 18:45:43 +0100

    :rocket: [Auto] - `dependencies rollup`.

--- scripts/dependencies.py
+++ scripts/dependencies.py
@@ -1,6 +1,7 @@
 import pathlib
 import subprocess
 import sys
+import typing
 
 
 def main() -> int:
@@ -12,29 +13,28 @@ def main() -> int:
 
 
 def poetry_update():
-    return _run_command("poetry update")
+    return _run_command(("poetry", "update"))
 
 
 def pre_commit_update():
-    return _run_command("pre-commit autoupdate --bleeding-edge")
+    return _run_command(("pre-commit", "autoupdate", "--bleeding-edge"))
 
 
 def commit_and_push():
     return (
-        _run_command("git add poetry.lock .pre-commit-config.yaml")
-        + _run_command("git commit -m :rocket: [Automatic] `dependencies rollups`.")
-        + _run_command("git push")
+        _run_command(("git", "add", "poetry.lock", ".pre-commit-config.yaml"))
+        + _run_command(("git", "commit", "-m", ":rocket: [Auto] - `dependencies rollup`."))
+        + _run_command(("git", "push"))
     )
 
 
-def _run_command(command: str) -> int:
+def _run_command(command: typing.Tuple[str, ...]) -> int:
     """
     Run a command and return the subprocess exit code.
     :param command: Command to run.
     :return:
     """
-    root = pathlib.Path(__file__).parent.parent
-    return subprocess.Popen(command, cwd=root, stdout=sys.stdout, stderr=subprocess.STDOUT).returncode
+    return subprocess.run(command, stdout=sys.stdout, stderr=subprocess.STDOUT).returncode
 
 
 if __name__ == "__main__":
