1d63655b32cfeb74ebf52bf6c20a367f791f01f1
symonk <jackofspaces@gmail.com>
2022-05-03 18:57:46 +0100

    remove concept of warning mode

--- asserto/_constants.py
+++ asserto/_constants.py
@@ -7,9 +7,7 @@ class AssertTypes:
     An encapsulation of assertion types.
         HARD: Instant failure, the default.
         SOFT: A collection of failures, compiled at the end of the test.
-        WARN: No failure of the test, omit a warning instead.
     """
 
     HARD = "hard"
     SOFT = "soft"
-    WARN = "warn"
