6291c0cfdaa5c2aa621a0cc35692b3dd9c18b9d2
symonk <jackofspaces@gmail.com>
2022-05-08 15:59:06 +0100

    remove unused modules

--- asserto/handlers/_meta.py
+++
@@ -1,6 +0,0 @@
-class Handler:
-    def __getattribute__(self, item):
-        attr = super().__getattribute__(item)
-        if callable(attr):
-            # The attribute is a bound method; automatically guard it by invoking accept()
-            self.accept()
