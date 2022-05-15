a2bb33e3b9f522bf2d6626e66de7a072ed28aac0
symonk <jackofspaces@gmail.com>
2022-05-15 17:12:42 +0100

    more enhancements for `Regex` assertions and happy path unit tests

--- asserto/handlers/_regex.py
+++ asserto/handlers/_regex.py
@@ -16,17 +16,20 @@ class RegexHandler:
     def __init__(self, actual: typing.Any) -> None:
         self.actual = actual
 
-    def match(self, pattern: RE_PATTERN_ALIAS, flags: RE_FLAGS_ALIAS = 0) -> bool:
+    def match(self, pattern: RE_PATTERN_ALIAS, flags: RE_FLAGS_ALIAS = 0) -> None:
         """Matches the beginning of a string"""
         if not re.match(pattern, self.actual, flags):
-            raise AssertionError(f"{self.actual} did not begin with pattern: {pattern}")
-        return re.match(pattern, self.actual, flags) is not None
+            raise AssertionError(f"{self.actual} did not begin with pattern: {pattern=}")
 
     def search(self, pattern: RE_PATTERN_ALIAS, flags: RE_FLAGS_ALIAS) -> None:
-        raise NotImplementedError
+        if re.search(pattern, self.actual, flags) is None:
+            raise AssertionError(f"{self.actual} did not contain any matches for: {pattern=}")
 
-    def full_match(self, pattern: RE_PATTERN_ALIAS, count: int, flags: RE_FLAGS_ALIAS) -> None:
-        raise NotImplementedError
+    def fullmatch(self, pattern: RE_PATTERN_ALIAS, flags: RE_FLAGS_ALIAS) -> None:
+        if re.fullmatch(pattern, self.actual, flags) is None:
+            raise AssertionError(f"{self.actual} was not matched entirely by: {pattern=}")
 
-    def findall(self, pattern: RE_PATTERN_ALIAS, flags: RE_FLAGS_ALIAS):
-        raise NotImplementedError
+    def findall(self, pattern: RE_PATTERN_ALIAS, count: int, flags: RE_FLAGS_ALIAS) -> None:
+        matches = re.findall(pattern, self.actual, flags)
+        if len(matches) != count:
+            raise AssertionError(f"{self.actual} had: {len(matches)} non overlapping occurrences for pattern: {pattern}, not: {count}")
--- tests/test_regex.py
+++ tests/test_regex.py
@@ -1,3 +1,5 @@
+import re
+
 import pytest
 
 
@@ -6,6 +8,21 @@ def test_regex_matches(asserto) -> None:
 
 
 def test_regex_non_match_raises(asserto) -> None:
-    with pytest.raises(AssertionError) as error:
+    with pytest.raises(AssertionError, match=re.escape("Hello World did not begin with pattern: pattern='^F$'")):
         asserto("Hello World").match(r"^F$")
-    asserto(error.value.args[0]).is_equal_to("Hello World did not begin with pattern: ^F$")
+
+
+def test_match(asserto):
+    asserto("hello world").match(r"hello")
+
+
+def test_search(asserto):
+    asserto("Hello World").search(r"World")
+
+
+def test_fullmatch(asserto):
+    asserto("Only Full Matches").fullmatch(r"Only Full .*")
+
+
+def test_findall(asserto):
+    asserto("oneoneoneXoneoneone").findall(r"one", count=6)
