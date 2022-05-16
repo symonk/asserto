05e13102591b088dcf5df09e33957e0286a9cdaa
symonk <jackofspaces@gmail.com>
2022-05-16 21:26:37 +0100

    toying with a `dispatch_and_assert` util method in `Handler`

--- asserto/handlers/_handler.py
+++ asserto/handlers/_handler.py
@@ -1,2 +1,9 @@
 class Handler:
-    ...
+    @staticmethod
+    def dispatch_and_raise(fn, expected, error, *args, **kwargs):
+        """
+        Dispatches a call to an underlying callable and if the return value of the function
+        is not equal to expected, raises an AssertionError.
+        """
+        if fn(*args, **kwargs) != expected:
+            raise AssertionError(error)
