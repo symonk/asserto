548b5e151d8106c44a7f4e8b6f39977ddcf32b7c
symonk <jackofspaces@gmail.com>
2022-06-16 19:22:56 +0100

    adding handlers for `files` & `dates`

---
+++ asserto/handlers/_date.py
@@ -0,0 +1,5 @@
+from ._base import Handler
+
+
+class DateHandler(Handler):
+    """ A Handler for managing date-esque data. """
---
+++ asserto/handlers/_files.py
@@ -0,0 +1,5 @@
+from ._base import Handler
+
+
+class FileHandler(Handler):
+    """ A handler for managing file related assertions """
