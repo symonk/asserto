1719d12b4b215aceeaf68705da3c39de8d8b234a
symonk <jackofspaces@gmail.com>
2023-07-02 19:17:32 +0100

    exposing `assert_that` in the public API; fix types on `asserto` fixture

--- asserto/__init__.py
+++ asserto/__init__.py
@@ -1,7 +1,15 @@
+from ._api import assert_that
 from ._api import asserto
 from ._api import register_assert
 from ._asserto import Asserto
 from ._exceptions import UnsupportedHandlerTypeError
 from ._warnings import NoAssertAttemptedWarning
 
-__all__ = ("asserto", "register_assert", "Asserto", "NoAssertAttemptedWarning", "UnsupportedHandlerTypeError")
+__all__ = (
+    "asserto",
+    "register_assert",
+    "Asserto",
+    "NoAssertAttemptedWarning",
+    "UnsupportedHandlerTypeError",
+    "assert_that",
+)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,11 @@
+import typing
+
 import pytest
 
-from asserto import asserto as assert_factory
+from asserto import Asserto
+from asserto import assert_that
 
 
 @pytest.fixture
-def asserto():
-    return assert_factory
+def asserto() -> typing.Callable[[], Asserto]:
+    return assert_that
