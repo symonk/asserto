c832286ad262313c9f38a36f00b4bb1067576ad9
symonk <jackofspaces@gmail.com>
2023-07-07 00:21:37 +0100

    remove `ensure_type_is`

--- asserto/mixins/_base.py
+++ asserto/mixins/_base.py
@@ -2,6 +2,3 @@
 
 class BaseMixin:
     """A Base mixin for useful functionality across all assertion mixins."""
-
-    def ensure_type_is():
-        """Ensures the type of the actual value is as expected."""
